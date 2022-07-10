ca91d3989d3b605b8eac5a0e41ea9bb96c7d5beb
michealyang <michealyang@aviagames.com>
2022-07-10 22:29:32 +0800

    update

--- sdk-tencent/sdk-tencent-mini/src/main/java/tech/qijin/sdk/tencent/mini/pojo/MsgValue.java
+++ sdk-tencent/sdk-tencent-mini/src/main/java/tech/qijin/sdk/tencent/mini/pojo/MsgValue.java
@@ -2,9 +2,24 @@ package tech.qijin.sdk.tencent.mini.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import tech.qijin.util4j.utils.DateUtil;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class MsgValue {
     private String value;
+
+    public static MsgValue of(Number v) {
+        return new MsgValue(String.valueOf(v));
+    }
+
+    public static MsgValue of(Date v) {
+        return new MsgValue(DateUtil.formatStr(v, DateUtil.DATETIME_FORMAT));
+    }
+
+    public static MsgValue of(String v) {
+        return new MsgValue(v);
+    }
 }
